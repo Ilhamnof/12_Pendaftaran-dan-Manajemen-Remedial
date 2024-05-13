fd78a5fe4eae415bf993e6020e45c1f1d9422dc8
VioniWijaya <viowijayaputri@gmail.com>
2024-05-13 14:06:37 +0700

    ubah password

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controller/auth.controller');
-const verifyToken = require('../middleware/verifyToken');
-const beforeLogin = require('../middleware/beforeLogin');  
+const controller = require("../controller/auth.controller");
+const verifyToken = require("../middleware/verifyToken");
+const beforeLogin = require("../middleware/beforeLogin");
 
-
-
-router.get('/login',beforeLogin, controller.form);
-router.post('/process-login',beforeLogin, controller.cekLogin);
-
-
-
-router.post('/logout', verifyToken, controller.logout);
-
+router.get("/login", beforeLogin, controller.form);
+router.get("/forgot-password", beforeLogin, controller.forgotPasswordForm);
+router.post("/confirm-email", beforeLogin, controller.confirmEmail);
+router.post("/process-login", beforeLogin, controller.cekLogin);
+router.get("/reset-password/:token", beforeLogin, controller.formResetPassword);
+router.post("/reset-password", beforeLogin, controller.resetPassword);
+router.post("/logout", verifyToken, controller.logout);
 
 module.exports = router;
