f92df20a41e3c34c0a673f93f415629d62405a1e
Ilham Nofaldi <81660180+Ilhamnof@users.noreply.github.com>
2024-06-03 00:23:24 +0700

    penambahan modal logout

--- routes/mahasiswa.route.js
+++ routes/mahasiswa.route.js
@@ -14,6 +14,10 @@ router.get('/ubah', verifyTokenAndRole('mahasiswa'), (req, res) => {
     res.render('ubahPw', { title: 'Ubah Password' }); // Menambahkan title
 });
 
+router.get('/formulir', verifyTokenAndRole('mahasiswa'),getMahasiswaData, (req, res) => {
+    res.render('formulir', { title: 'Formulir' }); // Menambahkan title
+});
+
 // Profil page
 router.get('/profil', verifyTokenAndRole('mahasiswa'), getMahasiswaData, (req, res) => {
     res.render('profil', { title: 'Profil'}); // Menambahkan title
