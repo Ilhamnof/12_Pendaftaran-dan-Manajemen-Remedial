229359a23ac7bd4c44ea03da89557448d5911b85
Ilham Nofaldi <81660180+Ilhamnof@users.noreply.github.com>
2024-06-08 18:12:36 +0700

    validasi login backend

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,36 +1,39 @@
 const { User } = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
 
 const form = (req, res) => {
-    return res.render("login");
+    return res.render("login", { errorMessages: [] });
 };
 
 const cekLogin = async (req, res) => {
     const { email, password } = req.body;
+
+    // Validasi request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map(error => error.msg);
+        return res.render("login", { errorMessages });
+    }
+
     try {
-        // Menggunakan nama variabel lain untuk menyimpan hasil pencarian user
         const user = await User.findOne({ where: { email } });
-
         if (!user) {
-            return res.redirect("/auth/login");
+            return res.render("login", { errorMessages: ["Email atau password tidak valid"] });
         }
 
-        // Verifikasi password
         const isValidPassword = await bcrypt.compare(password, user.password);
-
         if (!isValidPassword) {
-            return res.redirect("/auth/login");
+            return res.render("login", { errorMessages: ["Email atau password tidak valid"] });
         }
 
-        // Buat token JWT
         const token = jwt.sign(
             { id: user.id, email: user.email, role: user.role },
             "secretsecret",
             { expiresIn: 86400 }
         );
 
-        // Set cookie dengan token
         res.cookie("token", token, { httpOnly: true });
 
         // Redirect ke halaman sesuai dengan peran pengguna
@@ -42,7 +45,6 @@ const cekLogin = async (req, res) => {
             return res.redirect("/admin/dashboard");
         }
 
-        // Jika tidak ada peran yang cocok, berikan respons standar
         res.status(200).send({ auth: true, token: token });
     } catch (err) {
         console.error("Error during login: ", err);
@@ -58,27 +60,19 @@ function logout(req, res) {
 const ubahPassword = async (req, res) => {
     try {
         const { currentPassword, newPassword } = req.body;
-        
-        // Log data yang diterima dari form
-        console.log("Request body:", req.body);
 
-        // Cari pengguna berdasarkan userId
         const user = await User.findByPk(req.userId);
-        console.log(user);
         if (!user) {
             return res.status(404).json({ message: "Pengguna tidak ditemukan" });
         }
 
-        // Periksa apakah password saat ini cocok
         const isPasswordValid = await bcrypt.compare(currentPassword, user.password);
         if (!isPasswordValid) {
             return res.status(401).json({ message: "Password saat ini salah" });
         }
 
-        // Enkripsi password baru
         const hashedNewPassword = await bcrypt.hash(newPassword, 10);
 
-        // Perbarui password pengguna di database
         await user.update({ password: hashedNewPassword });
 
         return res.status(200).json({ message: "Password berhasil diubah" });
@@ -88,15 +82,9 @@ const ubahPassword = async (req, res) => {
     }
 };
 
-
-
-
-
-
 module.exports = {
     form,
     cekLogin,
     logout,
     ubahPassword
 };
-
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const controller = require("../controller/auth.controller");
 const verifyTokenAndRoles = require("../middleware/verifyTokenAndRole");
 const beforeLogin = require("../middleware/beforeLogin");
+const { body } = require("express-validator");
 
 
 router.get("/login", beforeLogin, controller.form);
-router.post("/process-login", controller.cekLogin);
+router.post("/process-login", 
+    [
+    body('email').isEmail().withMessage('Email tidak valid'),
+    body('password').isLength({ min: 3 }).withMessage('Password harus memiliki minimal 6 karakter')
+    ],
+    controller.cekLogin
+    );
 router.post("/logout", controller.logout);
 router.post("/ubahPassword", verifyTokenAndRoles(['mahasiswa', 'admin']), async (req, res) => {
     try {
